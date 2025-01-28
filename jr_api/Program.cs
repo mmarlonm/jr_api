3c7400c8bca847786528090708afdef7588ed7c2
marlon.garcia <marlon.garcia@grupoblueocean.com>
2025-01-28 13:07:55 -0600

    Initial commit

--- jr_api/Program.cs
+++ jr_api/Program.cs
@@ -70,5 +70,5 @@ app.UseAuthentication();
 app.UseAuthorization();
 
 app.MapControllers();
-
-app.Run();
+var port = Environment.GetEnvironmentVariable("PORT") ?? "5000";
+app.Run($"http://0.0.0.0:{port}");
--- jr_api/obj/Debug/net8.0/jr_api.AssemblyInfo.cs
+++ jr_api/obj/Debug/net8.0/jr_api.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("jr_api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3947731f0948a6fe400b9e64994e8ddfd1a4eae2")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+11caa1c498dc284629200eda5e15ed2e18858988")]
 [assembly: System.Reflection.AssemblyProductAttribute("jr_api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("jr_api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
